e2829b523dc450640bf7649fdd535f4254a0ea1e
Hannes Frederic Sowa <hannes@stressinduktion.org>
2013-06-17 01:15:35 +0200

    log NPEs

--- src/entropia/clubmonitor/Null.java
+++ src/entropia/clubmonitor/Null.java
@@ -1,11 +1,23 @@
 package entropia.clubmonitor;
 
 import org.eclipse.jdt.annotation.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Null {
+    private static final Logger logger = LoggerFactory.getLogger(Null.class);
+    private static final String ERROR_PREFIX = "null pointer assertion failed: ";
+    
     public static <T> T assertNonNull(@Nullable T t) {
-        if (t == null)
-            throw new NullPointerException();
+        if (t == null) {
+            final NullPointerException npe = new NullPointerException();
+            final StackTraceElement[] stes = npe.getStackTrace();
+            if (stes != null && stes.length >= 2 && stes[1] != null) {
+                logger.error(ERROR_PREFIX + stes[1].toString());
+            } else
+                logger.error(ERROR_PREFIX + "unkown location");
+            throw npe;
+        }
         return t;
     }
 }
