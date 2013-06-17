29f4bb5c85e04850c10949f8d9c72423f240c8a2
Hannes Frederic Sowa <hannes@stressinduktion.org>
2013-06-18 00:56:59 +0200

    cleanup imports

--- src/entropia/clubmonitor/FileHandlerThread.java
+++ src/entropia/clubmonitor/FileHandlerThread.java
@@ -3,7 +3,6 @@ package entropia.clubmonitor;
 import java.io.BufferedInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
