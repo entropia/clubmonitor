625f72b95b3b4cb3392052abd7696e567450f778
Hannes Frederic Sowa <hannes@stressinduktion.org>
2013-06-18 01:05:39 +0200

    correct indentation

--- src/entropia/clubmonitor/MpdNotifier.java
+++ src/entropia/clubmonitor/MpdNotifier.java
@@ -85,32 +85,30 @@ final class MpdNotifier extends PublicOnlyTrigger implements Runnable {
         }
         logger.info("connection check successful");
     }
-    
+
     @Override
     public void run() {
-	final String name = Thread.currentThread().getName();
+        final String name = Thread.currentThread().getName();
         while (!Thread.interrupted()) {
             try {
                 try (final Socket socket = initSocket();
                      final BufferedReader in = new BufferedReader(
-                             new InputStreamReader(socket.getInputStream()));
+                                new InputStreamReader(socket.getInputStream()));
                      final BufferedWriter out = new BufferedWriter(
-                             new OutputStreamWriter(
-                                     socket.getOutputStream()))) {
-                            checkHeader(in);
-                            checkConnection(in, out);
-                            while (true) {
-                                process(in, out);
-                            }
+                                new OutputStreamWriter(socket.getOutputStream()))) {
+                    checkHeader(in);
+                    checkConnection(in, out);
+                    while (true) {
+                        process(in, out);
+                    }
                 } catch (final IOException e) {
-                    logger.warn(String.format("exeption in %s. Sleeping for" +
-                    		" %d milliseconds.",
-                	    name, RETRY_TIMEOUT), e);
+                    logger.warn(String.format("exeption in %s. Sleeping for"
+                            + " %d milliseconds.", name, RETRY_TIMEOUT), e);
                     Thread.sleep(RETRY_TIMEOUT);
                     logger.warn(name + " is ready again for sending commands.");
                 }
             } catch (final InterruptedException e) {
-                    Thread.currentThread().interrupt();
+                Thread.currentThread().interrupt();
             }
         }
     }
